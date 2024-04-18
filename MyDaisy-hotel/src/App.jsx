219cc7b3e98c6f2be841e2efc7e65af091501c11
Quoc Hoang <hoangvanquoc923@gmail.com>
2024-04-18 10:14:24 +0700

    Add navbar and footer components

--- MyDaisy-hotel/src/App.jsx
+++ MyDaisy-hotel/src/App.jsx
@@ -3,21 +3,23 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom'
 import ExistingRoom from './components/room/ExistingRooms'
 import EditRoom from './components/room/EditRoom'
-import { Route, Routes } from "react-router-dom"
+import { Router, Route, Routes } from "react-router-dom"
 import HomePage from "./components/home/HomePage"
+import NavBar from "./components/layout/NavBar"
+import Footer from "./components/layout/Footer"
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/existing-rooms" element={<ExistingRoom />} />
         <Route path="/edit-room/:roomId" element={<EditRoom />} />
         <Route path="/new-room" element={<AddRoom />} />
       </Routes>
-      
-      
+      <Footer />  
     </>
   )
 }
---
+++ MyDaisy-hotel/src/components/layout/NavBar.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+    const [showAccount, setShowAccount] = useState(false)
+    const handleClickShowAccount = () => {
+        setShowAccount(prev => !prev)
+    }
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
+            <div className="container-fluid">
+                <Link to="/">
+                    <span className="hotel-color">My Daisy Hotel</span>
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarScroll"
+                    aria-controls="navbarScroll"
+                    aria-expanded="false"
+                    aira-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to={"/browse-all-rooms"}>
+                                Browse all rooms
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to={"/admin"}>
+                                Admin
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <ul className="d-flex navbar-nav align-items-center">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={"/find-booking"}>
+                                Find my booking
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-item dropdown">
+                            <a
+                                className={`btn dropdown-toggle ${showAccount ? "show" : ""}`}
+                                type="button"
+                                data-bs-target="#dropdownAccount"
+                                id="dropdownAccount"
+                                aria-expanded="false"
+                                onClick={handleClickShowAccount}
+                            >
+                                Account
+                            </a>
+                            <ul className={`dropdown-menu ${showAccount ? "show" : ""}`}
+                                aria-labelledby="dropdownAccount"
+                            >
+                                <li className="dropdown-item">
+                                    <Link to={"/login"}>Login</Link>
+                                </li>
+                                <li className="dropdown-item">
+                                    <Link to={"/profile"}>Profile</Link>
+                                </li>
+                                <div class="dropdown-divider"></div>
+                                <li className="dropdown-item">
+                                    <Link to={"/logout"}>Logout</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
